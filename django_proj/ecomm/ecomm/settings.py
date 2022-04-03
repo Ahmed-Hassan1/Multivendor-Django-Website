f5e2fe50e9a88302a4cf8028eda35aeafd41f2ab
Ahmed Mohammed Abdelaziz Hasan <ahmed.abdelaziz.hasan92@gmail.com>
2022-04-03 17:29:45 +0200

    Custom user creation

--- django_proj/ecomm/ecomm/settings.py
+++ django_proj/ecomm/ecomm/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     #new apps
     'store',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static')]
 
